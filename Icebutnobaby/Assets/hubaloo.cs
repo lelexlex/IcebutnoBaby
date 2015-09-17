2561d0a930893108c2eb24a653a6f3503ad478f5
Alex Walker <alex@admins-MBP-2.ad.uvu.edu>
2015-09-17 10:00:40 -0600

    no message

---
+++ Icebutnobaby/Assets/hubaloo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hubaloo : MonoBehaviour {
+
+	int nips = 76;
+
+	void Start () 
+	{
+		nips = cucu (nips);
+		Debug.Log (nips);
+	}
+
+	int cucu (int number)
+	{
+		int ret;
+		ret = number + 50;
+		return ret;
+	}
+}
