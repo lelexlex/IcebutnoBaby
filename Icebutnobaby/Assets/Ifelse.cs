2641edf8511cb984ae3f11fb6eb1ad609de20fc9
unknown <10619687@CS514-08.ad.uvu.edu>
2015-09-22 09:31:31 -0600

    no message

---
+++ Icebutnobaby/Assets/Ifelse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifelse : MonoBehaviour {
+
+	float mine=80;
+
+	//add float and getkeydown
+	void Start () 
+	
+	{
+
+	}
+
+	void Update () 
+		{
+		if (Input.GetKeyDown (KeyCode.Space))
+			Test ();
+
+
+		}
+
+
+		void Test()
+		{
+			if (mine > 54){
+				print ("I'm more than 54");
+			} else if (mine < 54) {
+				print ("I'm less than 54");
+			}
+		}
+}
