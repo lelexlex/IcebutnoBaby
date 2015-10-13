9af62ff906ba0224aba4f300242e8cacef6df54d
unknown <10619687@CS514-08.ad.uvu.edu>
2015-10-13 09:36:36 -0600

    Scope and access working

---
+++ Icebutnobaby/Assets/Snopesandaccess.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snopesandaccess : MonoBehaviour {
+
+
+	public int a1= 0;
+	
+
+	private Anotherclass rapgod;
+	// Use this for initialization
+	void Start () {
+		a1 = 42;
+	}
+
+	void example (int eminem, int god){
+		int answer;
+		answer = eminem * god * a1;
+		Debug.Log (answer);
+	}
+}
