ffd1e42e188bb96a9da00b208464e9dfab024e9d
unknown <10619687@CS514-08.ad.uvu.edu>
2015-10-27 10:07:12 -0600

    Basic Movement Snoop

---
+++ Duckhunt/Assets/Movement01.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement01 : MonoBehaviour {
+
+	public int speed = 5;
+
+
+	void Start () {
+		transform.position = new Vector3 (0, 0, 0);
+			print (transform.position.y);
+		}
+
+	void Update () {
+	 	float translation = Time.deltaTime * speed;
+		transform.Translate (translation, 0, 0);
+	}
+
+}
