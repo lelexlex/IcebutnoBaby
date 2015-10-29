1e2428ce9362f876d92963061f4f1619e3572068
unknown <10619687@CS514-08.ad.uvu.edu>
2015-10-29 09:33:57 -0600

    Enemy Diagonal Movement-Good

--- Duckhunt/Assets/Movement01.cs
+++ Duckhunt/Assets/Movement01.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class Movement01 : MonoBehaviour {
 
-	public int speed = 5;
+	public int speed = 6;
+	public int upspeed = 1;
 
 
 	void Start () {
-		transform.position = new Vector3 (0, 0, 0);
-			print (transform.position.y);
+		transform.position = new Vector3 (-12, 0, 0);
+			
 		}
 
 	void Update () {
+		float translation2 = Time.deltaTime * upspeed;
 	 	float translation = Time.deltaTime * speed;
+		transform.Translate (0, translation2, 0);
 		transform.Translate (translation, 0, 0);
 	}
 
