10dcd5d8025bc84a17f1693d7696898d05a0ccd7
unknown <10619687@CS514-08.ad.uvu.edu>
2015-11-03 09:47:39 -0700

    Spawning and Enemy Limit

--- Duckhunt/Assets/spawn.cs
+++ Duckhunt/Assets/spawn.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class spawn : MonoBehaviour {
 
+	public float spawntime = 4;
+	public Transform[] spawnpoints;
+	public GameObject snoop;
 
 
-	spawn Start () {
+	void Start () 
+	
+	{
+		snoop = GameObject.Find("snoop");
 
+		InvokeRepeating ("Spawn", spawntime, spawntime);
 	}
 
-	/*look into Vector 2 movement*/
+	void Spawn ()
+	{
+		int spawnpointindex = Random.Range (0, spawnpoints.Length);
 
 
-
+		Instantiate (snoop, spawnpoints [spawnpointindex].position, spawnpoints [spawnpointindex].rotation);
+	}
+\
+	void update ()
+	{
+		if enemycount
 }
